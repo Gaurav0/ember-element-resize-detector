4c7750e1bcd54861825e0a1a67a1e9411c907f34
Gaurav Munjal <gaurav@peeriq.com>
2019-01-23 14:05:40 -0500

    fix for engines

--- index.js
+++ index.js
@@ -9,11 +9,28 @@ var Funnel = require('broccoli-funnel');
 module.exports = {
   name: 'ember-element-resize-detector',
 
-  included: function(app) {
-    if (app.app) {
-      app = app.app;
+  included: function(app, parentAddon) {
+    this._super.apply(this, arguments);
+  
+    // Quick fix for add-on nesting
+    // https://github.com/aexmachina/ember-cli-sass/blob/v5.3.0/index.js#L73-L75
+    // see: https://github.com/ember-cli/ember-cli/issues/3718
+    while (app.app || app.parent) {
+      app = app.app || app.parent;
     }
-    this.app = app;
+
+    // if app.import and parentAddon are blank, we're probably being consumed by an in-repo-addon
+    // or engine, for which the "bust through" technique above does not work.
+    if (typeof app.import !== 'function' && !parentAddon) {
+      if (app.registry && app.registry.app) {
+        app = app.registry.app;
+      }
+    }
+
+    if (!parentAddon && typeof app.import !== 'function') {
+      throw new Error('ember-cli-resize-detector is being used within another addon or engine and is'
+        + ' having trouble registering itself to the parent application.');
+    }
 
     if (!process.env.EMBER_CLI_FASTBOOT) {
       app.import('vendor/element-resize-detector/element-resize-detector.js');
